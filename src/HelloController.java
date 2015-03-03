e0725f74280f805c6219ccd340156a0a986ea447
gifunogi <gifunogi@gmail.com>
2015-03-03 12:26:51 +0900

    Hello JavaFX

---
+++ src/HelloController.java
@@ -0,0 +1,20 @@
+/*
+ 
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+class HelloController {
+	@FXML private Button helloBUtton;
+	@FXML private Label helloLabel;
+	private int cnt;
+	@FXML
+	public void onHelloBUttonClicked(ActionEvent event) {
+		this.helloLabel.setText("Clicked! : " + cnt);
+		cnt++;
+	}
+}
+
+*/
