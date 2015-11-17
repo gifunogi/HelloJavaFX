e2976d6b721e200ba3986a030a9a35f4c2c01c91
gifunogi <gifunogi@gmail.com>
2015-11-17 21:28:19 +0900

    .gitignoreの追加

--- src/gifunogi/javafx/HelloController.java
+++ src/gifunogi/javafx/HelloController.java
@@ -5,16 +5,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class HelloController {
-	
-
-	@FXML private Button helloButton;
-	@FXML private Label helloLabel;
-	private int cnt;
+public class HelloController {	
+	@FXML
+	private Button helloButton;
+	@FXML
+	private Label helloLabel;
+	private int cnt = 0;
 	@FXML
 	public void onHelloButtonClicked(ActionEvent event) {
-		this.helloLabel.setText("Clicked! : " + cnt);
 		cnt++;
+		this.helloLabel.setText("Clicked! : " + cnt);
 	}
 }
 
